931b94c01b1f0cc90dc32221edace3286e27077a
Guilherme Costa <guicost98@gmail.com>
2023-08-30 15:41:41 -0300

    refactor: headerDesktop

--- src/components/HeaderDesktop.tsx
+++ src/components/HeaderDesktop.tsx
@@ -2,79 +2,38 @@ import React, { useState } from "react";
 import Logo from "../images/logo.svg";
 
 const HeaderDesktop = () => {
-  const hoveredStyle = "absolute w-full h-1 bg-orange-500 -bottom-[2.5rem]";
-  const [isHovered, setIsHovered] = useState<string>("");
+  const [isHovered, setIsHovered] = useState<number | null>(null);
 
-  const handleMouseOver = (e) => {
-    setIsHovered(e.target.id);
+  const handleMouseOver = (index: number) => {
+    setIsHovered(index);
   };
 
   const handleMouseOut = () => {
-    setIsHovered("");
+    setIsHovered(null);
   };
 
+  const menuItems = ["Collections", "Men", "Women", "About", "Contact"];
+
   return (
-    <div className="w-full py-10 flex items-center  border-b">
+    <div className="w-full py-10 flex items-center border-b">
       <div className="mr-12">
         <img src={Logo} alt="logo" />
       </div>
       <nav>
         <ul className="flex gap-10 text-stone-500">
-          <li className="hover:text-stone-800 relative">
-            <a
-              onMouseOver={handleMouseOver}
-              onMouseOut={handleMouseOut}
-              id="1"
-              href="#home"
-            >
-              Collections
-            </a>
-            <div className={isHovered === "1" ? hoveredStyle : ""}></div>
-          </li>
-          <li className="hover:text-stone-800 relative">
-            <a
-              onMouseOver={handleMouseOver}
-              onMouseOut={handleMouseOut}
-              id="2"
-              href="#home"
-            >
-              Men
-            </a>
-            <div className={isHovered === "2" ? hoveredStyle : ""}></div>
-          </li>
-          <li className="hover:text-stone-800 relative">
-            <a
-              onMouseOver={handleMouseOver}
-              onMouseOut={handleMouseOut}
-              id="3"
-              href="#home"
-            >
-              Women
-            </a>
-            <div className={isHovered === "3" ? hoveredStyle : ""}></div>
-          </li>
-          <li className="hover:text-stone-800 relative">
-            <a
-              onMouseOver={handleMouseOver}
-              onMouseOut={handleMouseOut}
-              id="4"
-              href="#home"
-            >
-              About
-            </a>
-            <div className={isHovered === "4" ? hoveredStyle : ""}></div>
-          </li>
-          <li className="hover:text-stone-800 relative">
-            <a
-              onMouseOver={handleMouseOver}
-              onMouseOut={handleMouseOut}
-              id="5"
-              href="#home"
-            >
-              Contact
-            </a>
-            <div className={isHovered === "5" ? hoveredStyle : ""}></div>
-          </li>
+          {menuItems.map((item, index) => (
+            <li className="hover:text-stone-800 relative" key={index}>
+              <button
+                onMouseOver={() => handleMouseOver(index)}
+                onMouseOut={handleMouseOut}
+              >
+                {item}
+              </button>
+              {isHovered === index && (
+                <div className="absolute w-full h-1 bg-orange-500 -bottom-[2.5rem]"></div>
+              )}
+            </li>
+          ))}
         </ul>
       </nav>
     </div>
